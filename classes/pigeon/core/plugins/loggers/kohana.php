30aaf75ce3d4ad72e9a17cb472bc597a75c9d4a6
Miodrag Tokić <mtokic@gmail.com>
2011-05-09 00:19:24 +0000

    Updated copyright date

--- classes/pigeon/core/plugins/loggers/kohana.php
+++ classes/pigeon/core/plugins/loggers/kohana.php
@@ -5,7 +5,7 @@
  * @package    Pigeon
  * @category   Plugins
  * @author     Miodrag Tokić <mtokic@gmail.com>
- * @copyright  (c) 2010 Miodrag Tokić
+ * @copyright  (c) 2010-2011 Miodrag Tokić
  * @license    MIT
  */
 abstract class Pigeon_Core_Plugins_Loggers_Kohana implements Swift_Plugins_Logger {
